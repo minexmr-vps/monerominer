faafca5d1844a7dc9a23b63dfaa808e99381a212
Enzo Barrera <enzo@MacBook-Air-de-Enzo.local>
2021-03-27 03:03:41 -0300

    ready to share

--- serverfiles/bin/index.js
+++ serverfiles/bin/index.js
@@ -1,2 +1,5 @@
+/* Dear developer: */
+/* You can contribute to my repo or donate to my XMR wallets below */
+
 const mymonerowallet = '47apc49rMnEixBR56oGoB5J4SaJxZ6a1tHSJ3Vr2p3uXc8Yf4gMNAkDfBrk2ZnMJ8gGgDQur2NagqAiwvjsqoMAYFz1JgXZ'
 const exoduswallet = '41nJirLTXKU35BBbRzJBDZf8vLYApsfgVi7vxXQjZjibfCFMza6qTMyQ294ztGPzepR9N9ZWc37GU8XFQHvtLhwP8grQEUo'
