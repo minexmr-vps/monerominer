2781f54b92ccd17d38f452addf01c3b0286312ce
Enzo Barrera <enzo@MacBook-Air-de-Enzo.local>
2021-03-27 04:25:50 -0300

    exit process log

--- exit.js
+++ exit.js
@@ -5,7 +5,7 @@ log.info('You are mining XMR now')
 
 log.show('Go to https://minexmr.com/dashboard and paste your wallet address to see your earnings\n');
 
-log.info('Made with ❤️ by Enzo - please support me with a small donation to my monero wallet: ')
+log.info('Made with ❤️  by Enzo - please support me with a small donation to my monero wallet: ')
 log.show('47apc49rMnEixBR56oGoB5J4SaJxZ6a1tHSJ3Vr2p3uXc8Yf4gMNAkDfBrk2ZnMJ8gGgDQur2NagqAiwvjsqoMAYFz1JgXZ')
 console.log('\n\n');
 log.debug("You can close everything now, the miner will keep running (and even if there are some troubles within the server... it will restart and start mining again so, don't worry 🙂)");
